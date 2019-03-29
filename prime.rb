1f39537475dc410d4cac242176d90c2202e548ce
James Cox <jamesncox@yahoo.com>
2019-03-29 18:59:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,22 @@
-def prime?(number)
-  if number >= 2
-    (2..number - 1).all? do |x|
-      number % x != 0
-    end
-  else
-    return false
-  end
-end
+# <<<<<<< HEAD
+# def prime?(number)
+#   if number >= 2
+#     (2..number - 1).all? do |x|
+#       number % x != 0
+#     end
+#   else
+#     return false
+#   end
+# end
+# =======
+# def prime?(number)
+#   for d in 2..(number - 1)
+#   if (number % d) == 0
+#     return false
+#   end
+#   end
+# >>>>>>> e44adcfc5bce1fd1bc404490f0c073f439cebf9e
+
+def prime?
+  
+    
